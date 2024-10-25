a193cbd20272181dcb6466635d731a94eed06845
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-10-25 12:41:51 +0100

    Refactor UserMapper to add method for converting User entity to UserDto

--- src/main/java/org/example/mapper/UserMapper.java
+++ src/main/java/org/example/mapper/UserMapper.java
@@ -1,4 +1,19 @@
 package org.example.mapper;
 
+import org.example.dto.UserDto;
+import org.example.entities.User;
+
 public class UserMapper {
+
+    public static UserDto toUserDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getIdentificationDocument(),
+                user.getNationality(),
+                user.getRegistrationDate(),
+                user.getExpirationDate()
+        );
+    }
 }
