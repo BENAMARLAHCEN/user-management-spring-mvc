100ac294c235a5349958e5a02336cf7a33f35f6a
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-10-25 12:41:42 +0100

    Refactor UserDto class to use record syntax and add input validation

--- src/main/java/org/example/dto/UserDto.java
+++ src/main/java/org/example/dto/UserDto.java
@@ -1,4 +1,37 @@
 package org.example.dto;
 
-public class UserDto {
+import java.time.LocalDate;
+
+public record UserDto(
+        Long id,
+        String firstName,
+        String lastName,
+        String identificationDocument,
+        String nationality,
+        LocalDate registrationDate,
+        LocalDate expirationDate
+){
+    public UserDto {
+        if (firstName == null || firstName.isBlank()) {
+            throw new IllegalArgumentException("First name is required");
+        }
+        if (lastName == null || lastName.isBlank()) {
+            throw new IllegalArgumentException("Last name is required");
+        }
+        if (identificationDocument == null || identificationDocument.isBlank()) {
+            throw new IllegalArgumentException("Identification document is required");
+        }
+        if (nationality == null || nationality.isBlank()) {
+            throw new IllegalArgumentException("nationality is required");
+        }
+        if (registrationDate == null) {
+            throw new IllegalArgumentException("Registration date is required");
+        }
+        if (expirationDate == null) {
+            throw new IllegalArgumentException("Expiration date is required");
+        }
+        if (id != null && id < 0) {
+            throw new IllegalArgumentException("Id must be greater than zero");
+        }
+    }
 }
