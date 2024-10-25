a97d1c30e445cc261438a4bc64f0ffcb19fe452b
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-10-25 11:31:50 +0100

    Refactor UserController to fetch and display all users

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -1,17 +1,32 @@
 package org.example.controllers;
 
+import org.example.entities.User;
 import org.example.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
 
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
         return "users";
     }
 }
