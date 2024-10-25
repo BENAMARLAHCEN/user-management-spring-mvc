f9b70ca86f5c4f2831e4a37a5deb80c4a4c43228
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-10-25 11:31:45 +0100

    Refactor UserService interface and UserServiceImpl class

--- src/main/java/org/example/services/UserServiceImpl.java
+++ src/main/java/org/example/services/UserServiceImpl.java
@@ -7,4 +7,30 @@ import java.util.List;
 import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
 }
